6e7b9ff0102e2ce4278c51fc5c634fba182a03af
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-09 10:38:44 -0600

    Update function to add comment and add form listener

--- src/modules/popup/comment.js
+++ src/modules/popup/comment.js
@@ -21,22 +21,24 @@ export default class Comment {
     return res;
   };
 
-  addComments = async () => {
+  addComments = async (formObj) => {
+    const {
+      username,
+      comment,
+      id,
+    } = formObj;
+
     const url = `${this.requestURL}/comments`;
-    const res = await fetch(url, {
+    await fetch(url, {
       method: 'POST',
       body: JSON.stringify({
-        item_id: '79971',
-        username: 'Jane',
-        comment: 'Hello',
+        item_id: id,
+        username,
+        comment,
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
-    }).then((data) => data.json());
-
-    return res;
+    });
   };
-
-  // TODO: ADD NEW COMMENT INPUT
 }
--- src/modules/popup/popup.js
+++ src/modules/popup/popup.js
@@ -13,6 +13,7 @@ export default class Popup extends Comment {
         async (resp) => {
           await this.#displayPopup(resp);
           this.#closePopUp();
+          this.#addFormListener();
           // await this.addComments();
         },
       );
@@ -57,7 +58,7 @@ export default class Popup extends Comment {
           <h3>Add a comment</h3>
           <input type="text" name="username" id="name" placeholder="Your name" maxlength="30" required>
           <textarea name="comment" id="new-comment" placeholder="Your insights" cols="30" rows="10" maxlength="260" required></textarea>
-          <button type="submit" id="submit-comment">Comment</button>
+          <button type="submit" id="submit-comment" data-id="${resp.id}">Comment</button>
         </form>
       </div>
     `;
@@ -97,4 +98,23 @@ export default class Popup extends Comment {
     commentsBlock.append(commentsTitile, commentsList);
     return commentsBlock;
   };
+
+  // add comment listener
+  #addFormListener = () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const username = document.getElementById('name').value;
+      const comment = document.getElementById('new-comment').value;
+      const button = document.getElementById('submit-comment');
+      const id = button.getAttribute('data-id');
+
+      this.addComments({
+        username,
+        comment,
+        id,
+      }).then(() => console.log('it worked'));
+      form.reset();
+    });
+  }
 }
