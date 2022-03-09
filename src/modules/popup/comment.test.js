471ca8bbbb678835af8619614e21b1fddb16bcb9
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-09 15:52:51 -0600

    Add api mock and test comments counter

--- src/modules/popup/comment.test.js
+++ src/modules/popup/comment.test.js
@@ -2,8 +2,15 @@ import Comment from './comment.js';
 
 jest.mock('./comment.js');
 
-test('should print number of comments from the api', async () => {
+describe('Test comment counter', () => {
   const comment = new Comment();
-  const comments = await comment.getCommentsFromApi();
-  expect(comment.commentsCounter(comments)).toBe(5);
+  test('should print number of comments from the api', async () => {
+    const comments = await comment.getCommentsFromApi();
+    expect(comment.commentsCounter(comments)).toBe(5);
+  });
+
+  test('in case of an empty comments, should return 0', () => {
+    const comments = [];
+    expect(comment.commentsCounter(comments)).toBe(0);
+  });
 });
